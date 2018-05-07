6073dd8fc616bba3311144df9feb0581d1e5ee0b
Gabriel Taillon <gabriel.taillon@gmail.com>
2018-05-07 21:40:50 +0900

    QuadCounts Implemented tested and working. Ks2d2s is being implemented.

--- KS2D.py
+++ KS2D.py
@@ -1,4 +1,101 @@
 # Code créé par Gabriel Taillon le 7 Mai 2018
 #  2D Kolmogorov-Smyrnov Test. Exclusively Data-Data for now.
-import numpy as np
+# References: 
+#  [1] Peacock, J. A. (1983). Two-dimensional goodness-of-fit testing in astronomy. Monthly Notices of the Royal Astronomical Society, 202(3), 615-627.
+#  [2] Fasano, G., & Franceschini, A. (1987). A multidimensional version of the Kolmogorov–Smirnov test. Monthly Notices of the Royal Astronomical Society, 225(1), 155-170.
+#  [3]  Flannery, B. P., Press, W. H., Teukolsky, S. A., & Vetterling, W. (1992). Numerical recipes in C. Press Syndicate of the University of Cambridge, New York, 24, 78.
+import sys, os, numpy as np, scipy.stats
 import matplotlib.pyplot as plt
+Scriptpath=os.path.dirname(os.path.abspath(__file__)) # Path of this script
+Path2Res=os.path.join(Scriptpath,os.path.splitext(os.path.basename(__file__))[0])
+if not os.path.exists(Path2Res): os.makedirs(Path2Res)
+
+testdata1=np.random.uniform(size=(100,2))
+testdata2=np.random.uniform(size=(100,2))
+testdata3=np.random.uniform(0.2,0.5,size=(100,2))
+testlist1=np.random.uniform(size=(100,2)).tolist()
+testlist2=np.random.uniform(size=(100,2)).T.tolist()
+
+def CountQuads(Arr2D,point,silent=1,plotcheck=0):
+    # This function counts the number of points of Arr2D in each quadrant defined by straight lines crossing point.
+    # A bit of checking. if Arr2D and point are not lists or ndarray, exit.
+    if isinstance(point, list):
+        if not silent: print('point is a list')
+        pass
+    elif type(point).__module__+type(point).__name__=='numpyndarray':
+        point=np.ravel(point.copy())
+        if not silent: print('point is a numpy.ndarray')
+    else:
+        if not silent: print('point is neither a list not a numpy.ndarray. Exiting.')
+        return
+        
+    if isinstance(Arr2D, list):
+        if not silent: print('Arr2D is a list')
+        Arr2D=np.asarray((Arr2D))
+    elif type(Arr2D).__module__+type(Arr2D).__name__=='numpyndarray':
+        if not silent: print('Arr2D is a ndarray')
+    else:
+        if not silent: print('Arr2D is neither a list not a numpy.ndarray. Exiting.')
+        return
+    if Arr2D.shape[1]>Arr2D.shape[0]:
+        Arr2D=Arr2D.copy().T
+    
+    # The pp of Qpp refer to p for 'positive' and m for 'negative' quadrants. In order. first subscript is x, second is y.
+    Qpp=Arr2D[(Arr2D[:,0]>=point[0])&(Arr2D[:,1]>=point[1]),:]
+    Qmp=Arr2D[(Arr2D[:,0]<=point[0])&(Arr2D[:,1]>=point[1]),:]
+    Qpm=Arr2D[(Arr2D[:,0]>=point[0])&(Arr2D[:,1]<=point[1]),:]
+    Qmm=Arr2D[(Arr2D[:,0]<=point[0])&(Arr2D[:,1]<=point[1]),:]
+    if not silent: print('Same number of points in Arr2D as in all Quadrants: '+str((len(Qpp)+len(Qmp)+len(Qpm)+len(Qmm))==len(Arr2D)))
+    if plotcheck:
+        plt.plot(Arr2D[:,0],Arr2D[:,1],'.k')
+        plt.savefig(os.path.join(Path2Res,'QuadCheckArr2D.png'), bbox_inches='tight',dpi=500, edgecolor='w')
+        plt.plot(Qpp[:,0],Qpp[:,1],'.b')
+        plt.plot(Qmp[:,0],Qmp[:,1],'.g')
+        plt.plot(Qpm[:,0],Qpm[:,1],'.c')
+        plt.plot(Qmm[:,0],Qmm[:,1],'.y')
+        plt.plot(point[0],point[1],'.r')
+        plt.savefig(os.path.join(Path2Res,'QuadCheck.png'), bbox_inches='tight',dpi=500, edgecolor='w')
+        plt.close('all')
+    
+    # Normalized fractions:
+    ff=1./len(Arr2D)
+    fpp=len(Qpp)*ff
+    fmp=len(Qmp)*ff
+    fpm=len(Qpm)*ff
+    fmm=len(Qmm)*ff
+    return(fpp,fmp,fpm,fmm)
+R1=scipy.stats.pearsonr(testdata1[:,0],testdata1[:,1])[0]
+R2=scipy.stats.pearsonr(testdata2[:,0],testdata2[:,1])[0]
+R3=scipy.stats.pearsonr(testdata3[:,0],testdata3[:,1])[0]
+def ks2d2s(Arr2D1,Arr2D2):
+    d1,d2=0.,0.
+    for point1 in Arr2D1:
+        fpp1,fmp1,fpm1,fmm1=CountQuads(Arr2D1,point1)
+        fpp2,fmp2,fpm2,fmm2=CountQuads(Arr2D2,point1)
+        d1=max(d1,abs(fpp1-fpp2))
+        d1=max(d1,abs(fpm1-fpm2))
+        d1=max(d1,abs(fmp1-fmp2))
+        d1=max(d1,abs(fmm1-fmm2))
+    for point2 in Arr2D2:
+        fpp1,fmp1,fpm1,fmm1=CountQuads(Arr2D1,point2)
+        fpp2,fmp2,fpm2,fmm2=CountQuads(Arr2D2,point2)
+        d2=max(d2,abs(fpp1-fpp2))
+        d2=max(d2,abs(fpm1-fpm2))
+        d2=max(d2,abs(fmp1-fmp2))
+        d2=max(d2,abs(fmm1-fmm2))
+    print(d1,d2)
+    d=(d1+d2)/2.
+    sys.exit()
+    return()
+ks2d2s(testdata1,testdata2)
+print(R1,R2,R3)
+
+# Test list:
+CountQuads(testdata1,[0.1,0.5]) 
+CountQuads(testdata1,'a') 
+CountQuads('1',[0.1,0.5]) 
+CountQuads(testlist1,[0.1,0.5]) 
+CountQuads(testlist2,[0.1,0.5]) 
+# Test ndarray
+CountQuads(testdata1,np.array([0.1,0.5])) 
+CountQuads(testdata1.T,np.array([[0.1],[0.5]])) 
