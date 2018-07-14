0bf3b3daea597dc96177081706d2d0a558eaf49e
Gabriel Taillon <ga.taillon@gmail.com>
2018-07-14 13:44:58 +0900

    Remove NHPPMAngkuEst from init. Add one more test for NHPPLeemisEst.

--- stochastic/analysis/__init__.py
+++ stochastic/analysis/__init__.py
@@ -1,6 +1,5 @@
 # flake8: noqa
 from stochastic.analysis.estimation import NHPPLeemisEst
-from stochastic.analysis.estimation import NHPPMangkuEst
 from stochastic.analysis.KS2D import ks2d1s
 from stochastic.analysis.KS2D import ks2d2s
 
--- tests/analysis/test_estimation.py
+++ tests/analysis/test_estimation.py
@@ -8,4 +8,5 @@ def test_NHPPLeemisEst(AllReal2D):
     AllEventsT, GammaEstNoNan =  NHPPLeemisEst(AllReal2D)
     assert isinstance(AllEventsT, np.ndarray)
     assert isinstance(GammaEstNoNan, np.ndarray)
+    assert len(GammaEstNoNan)==len(AllEventsT)
     
