eb4d287b6265bf04e0ef3db6875fc015f2585f5f
Gabriel Taillon <ga.taillon@gmail.com>
2018-07-27 21:18:40 +0900

    Take files from dev.

---
+++ tests/continuous/test_nh_poisson_process.py
@@ -0,0 +1,24 @@
+"""Test NHPP."""
+# flake8: noqa
+import pytest
+
+from stochastic.continuous import NHPP
+
+def test_poisson_process_str_repr(lambdaa,boundaries):
+    instance = NHPP(lambdaa,boundaries)
+    assert isinstance(repr(instance), str)
+    assert isinstance(str(instance), str)
+
+def test_poisson_process_sample(lambdaa,boundaries, n_fixture):
+    instance = NHPP(lambdaa,boundaries)
+    if n_fixture is None:
+        with pytest.raises(ValueError):
+            s = instance.sample(n_fixture)
+    else:  # n_fixture is not None:
+        s = instance.sample(n_fixture)
+        assert len(s) == n_fixture 
+
+def test_poisson_process_times(lambdaa,boundaries, n):
+    instance = NHPP(lambdaa,boundaries)
+    with pytest.raises(AttributeError):
+        times = instance.times(n)
