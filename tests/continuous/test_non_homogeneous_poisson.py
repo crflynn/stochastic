49defc00bcedf20bb99c51770777aec9e927d3da
Gabriel Taillon <ga.taillon@gmail.com>
2018-08-03 18:38:44 +0900

    Added tests for multiple dimensions with lots of fixtures.

--- tests/continuous/test_non_homogeneous_poisson.py
+++ tests/continuous/test_non_homogeneous_poisson.py
@@ -1,24 +1,59 @@
-"""Test NHPP."""
+"""Test NonHomogeneousPoissonProcess."""
 # flake8: noqa
 import pytest
 
-from stochastic.continuous import NHPP
+from stochastic.continuous import NonHomogeneousPoissonProcess
 
-def test_non_homogeneous_poisson_process_str_repr(lambdaa,boundaries):
-    instance = NHPP(lambdaa,boundaries)
-    assert isinstance(repr(instance), str)
-    assert isinstance(str(instance), str)
+def test_non_homogeneous_poisson_process_str_repr(
+        lambda_func1D, lambda_arr1D, boundaries1D, 
+        lambda_func2D, lambda_arr2D, boundaries2D, 
+        lambda_func3D, lambda_arr3D, boundaries3D):
+    instance_func1D = NonHomogeneousPoissonProcess(lambda_func1D,boundaries1D)
+    instance_func2D = NonHomogeneousPoissonProcess(lambda_func2D,boundaries2D)
+    instance_func3D = NonHomogeneousPoissonProcess(lambda_func3D,boundaries3D)
+    instance_arr1D = NonHomogeneousPoissonProcess(lambda_arr1D,boundaries1D)
+    instance_arr2D = NonHomogeneousPoissonProcess(lambda_arr2D,boundaries2D)
+    instance_arr3D = NonHomogeneousPoissonProcess(lambda_arr3D,boundaries3D)
+    instances=(instance_func1D, instance_func2D, instance_func3D,
+        instance_arr1D, instance_arr2D, instance_arr3D)
+    for instance in instances:
+        assert isinstance(repr(instance), str)
+        assert isinstance(str(instance), str)
 
-def test_non_homogeneous_poisson_process_sample(lambdaa,boundaries, n_fixture):
-    instance = NHPP(lambdaa,boundaries)
-    if n_fixture is None:
-        with pytest.raises(ValueError):
+def test_non_homogeneous_poisson_process_sample(
+        lambda_func1D, lambda_arr1D, boundaries1D, 
+        lambda_func2D, lambda_arr2D, boundaries2D, 
+        lambda_func3D, lambda_arr3D, boundaries3D,
+        n_fixture):
+    instance_func1D = NonHomogeneousPoissonProcess(lambda_func1D,boundaries1D)
+    instance_func2D = NonHomogeneousPoissonProcess(lambda_func2D,boundaries2D)
+    instance_func3D = NonHomogeneousPoissonProcess(lambda_func3D,boundaries3D)
+    instance_arr1D = NonHomogeneousPoissonProcess(lambda_arr1D,boundaries1D)
+    instance_arr2D = NonHomogeneousPoissonProcess(lambda_arr2D,boundaries2D)
+    instance_arr3D = NonHomogeneousPoissonProcess(lambda_arr3D,boundaries3D)
+    instances=(instance_func1D, instance_func2D, instance_func3D,
+        instance_arr1D, instance_arr2D, instance_arr3D)
+    for instance in instances:
+        if n_fixture is None:
+            with pytest.raises(ValueError):
+                s = instance.sample(n_fixture)
+        else:  # n_fixture is not None:
             s = instance.sample(n_fixture)
-    else:  # n_fixture is not None:
-        s = instance.sample(n_fixture)
-        assert len(s) == n_fixture 
-
-def test_non_homogeneous_poisson_process_times(lambdaa,boundaries, n):
-    instance = NHPP(lambdaa,boundaries)
-    with pytest.raises(AttributeError):
-        times = instance.times(n)
+            assert len(s) == n_fixture 
+        
+def test_non_homogeneous_poisson_process_times(
+        lambda_func1D, lambda_arr1D, boundaries1D, 
+        lambda_func2D, lambda_arr2D, boundaries2D, 
+        lambda_func3D, lambda_arr3D, boundaries3D,
+        n):
+    instance_func1D = NonHomogeneousPoissonProcess(lambda_func1D,boundaries1D)
+    instance_func2D = NonHomogeneousPoissonProcess(lambda_func2D,boundaries2D)
+    instance_func3D = NonHomogeneousPoissonProcess(lambda_func3D,boundaries3D)
+    instance_arr1D = NonHomogeneousPoissonProcess(lambda_arr1D,boundaries1D)
+    instance_arr2D = NonHomogeneousPoissonProcess(lambda_arr2D,boundaries2D)
+    instance_arr3D = NonHomogeneousPoissonProcess(lambda_arr3D,boundaries3D)
+    instances=(instance_func1D, instance_func2D, instance_func3D,
+        instance_arr1D, instance_arr2D, instance_arr3D)
+    for instance in instances:
+        with pytest.raises(AttributeError):
+            times = instance.times(n)
